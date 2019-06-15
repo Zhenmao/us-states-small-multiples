a19992bf3ff41109580de91899d67b37c6990fec
Zhenmao Wan <wanzhenmao@gmail.com>
2019-06-15 14:50:44 +0800

    Add grid

--- main.js
+++ main.js
@@ -17,4 +17,67 @@ Promise.all([
 		}, [])
 	}));
 	console.log(data);
+
+	////////////////////////////////////////////////////////////
+	//// Setup /////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////
+	const margin = { top: 8, right: 8, bottom: 32, left: 56 };
+	const gridWidth = 80;
+	const gridHeight = 80;
+	const gridMargin = { top: 20, right: 4, bottom: 4, left: 4 };
+	const smallWidth = gridWidth - gridMargin.left - gridMargin.right;
+	const smallHeight = gridHeight - gridMargin.top - gridMargin.bottom;
+	const totalColumns = d3.max(data, d => d.x) + 1;
+	const totalRows = d3.max(data, d => d.y) + 1;
+	const width = totalColumns * gridWidth;
+	const height = totalRows * gridHeight;
+	const svgWidth = width + margin.left + margin.right;
+	const svgHeight = height + margin.top + margin.bottom;
+
+	const xGrid = d3
+		.scaleBand()
+		.domain(d3.range(totalColumns))
+		.range([0, width]);
+
+	const yGrid = d3
+		.scaleBand()
+		.domain(d3.range(totalRows))
+		.range([0, height]);
+
+	const svg = d3
+		.select(".chart")
+		.style("max-width", svgWidth)
+		.attr("viewBox", [0, 0, svgWidth, svgHeight]);
+
+	const g = svg
+		.append("g")
+		.attr("transform", `translate(${margin.left},${margin.top})`);
+
+	////////////////////////////////////////////////////////////
+	//// Render ////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////
+	const grid = g
+		.selectAll(".grid")
+		.data(data)
+		.join("g")
+		.attr("class", "grid")
+		.attr("transform", d => `translate(${xGrid(d.x)},${yGrid(d.y)})`);
+
+	const gGrid = grid
+		.append("g")
+		.attr("transform", `translate(${gridMargin.left},${gridMargin.top})`);
+
+	gGrid
+		.append("text")
+		.attr("class", "grid-title")
+		.attr("x", smallWidth / 2)
+		.attr("y", -4)
+		.attr("text-anchor", "middle")
+		.text(d => d.abbr);
+
+	gGrid
+		.append("rect")
+		.attr("class", "grid-rect")
+		.attr("width", smallWidth)
+		.attr("height", smallHeight);
 });
